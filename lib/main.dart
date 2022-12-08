e6a93d7490cc7bb0eacf83e2dde52d05c87f39b4
uglyonlytoday <superjang3357@naver.com>
2022-12-08 15:21:40 +0900

    fullscreen when start

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:carororo_dagga/pages/home_page.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
   runApp(
     GetMaterialApp(
       title: '카로로로 닦아!',
